0dcba90f26cebc89f9a9a2df722d384bb91e8084
Ben Lardie <benlardie@gmail.com>
2019-05-07 22:22:06 -0400

    started boiler plate and navbar working on smooth scroll with javascript

---
+++ main.js
@@ -0,0 +1,39 @@
+const navbarMenu = document.querySelector('.navbar ul');
+const navbarLinks = document.querySelectorAll('.navbar a');
+
+console.log("hello")
+
+for (let i =0; i <navbarLinks.length; i ++) {
+    navbarLinks[i].addEventListener('click', navbarLinkClick);
+  }
+
+function navbarLinkClick(event) {
+    smoothScroll(event);
+};
+
+function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute("href")==="#" ? "header" : event.currentTarget.getAttribute("href");
+    const targetPosition = document.querySelector(targetId).offsetTop;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition
+    const duration = 1000;
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    // window.scrollTo(0, distance*(progress/duration) + startPosition);
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+    }
+  }
+
+  function easeInOutCubic(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t + b;
+	t -= 2;
+    return c/2*(t*t*t + 2) + b;
+};
